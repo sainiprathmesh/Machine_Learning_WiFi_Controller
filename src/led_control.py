7cd1ea1559da3af38760c92dfb10da1292571085
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-04-12 22:03:43 +0530

    input choice taken and on and operation performed

--- src/led_control.py
+++ src/led_control.py
@@ -3,4 +3,11 @@ from boltiot import Bolt
 api_key = "a425c725-e879-4e29-a423-a9fd223e9e09"
 device_id = "BOLT14883313"
 mybolt = Bolt(api_key, device_id)
-print("_____MENU_____")
+print("_____MENU_____\n1.ON\n2.OFF")
+s = input()
+if s == "1":
+    response = mybolt.digitalWrite('0', 'HIGH')
+    print(response)
+elif s == "2":
+    response1 = mybolt.digitalWrite('0', 'LOW')
+    print(response1)
