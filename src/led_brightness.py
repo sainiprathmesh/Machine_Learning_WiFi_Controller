a3a261536e366de484f82dc72531441dadb39c32
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-04-12 22:40:06 +0530

    change led brightness using analog

--- src/led_brightness.py
+++ src/led_brightness.py
@@ -3,3 +3,5 @@ from boltiot import Bolt
 api_key = "a425c725-e879-4e29-a423-a9fd223e9e09"
 device_id = "BOLT14883313"
 mybolt = Bolt(api_key, device_id)
+response = mybolt.analogWrite('0', '10')
+print(response)
