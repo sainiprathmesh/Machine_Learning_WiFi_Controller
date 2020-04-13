ce29f7975ba659e74c82fc4a77b437dc045f047f
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-04-13 20:44:14 +0530

    object created for Mail

--- src/temp_email.py
+++ src/temp_email.py
@@ -1,7 +1,8 @@
-from boltiot import Bolt
+from boltiot import Bolt, Email
 
 from src import email_conf
 
 minimum_limit = 300
 maximum_limit = 600
 mybolt = Bolt(email_conf.API_KEY, email_conf.DEVICE_ID)
+mailer = Email(email_conf.MAILGUN_API_KEY, email_conf.SANDBOX_URL, email_conf.SENDER_EMAIL, email_conf.RECIPIENT_EMAIL)
