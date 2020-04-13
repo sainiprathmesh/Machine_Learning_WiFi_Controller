c010244cfc0d574cfa77915c117339654dbe07f5
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-04-13 19:51:24 +0530

    value checking and sending sms if value is out of range

--- src/temp_sms.py
+++ src/temp_sms.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from boltiot import Bolt, Sms
 
@@ -13,3 +14,14 @@ while True:
     response = mybolt.analogRead('A0')
     data = json.loads(response)
     print("Sensor value is: " + str(data['value']))
+    try:
+        sensor_value = int(data['value'])
+        if sensor_value > maximum_limit or sensor_value < minimum_limit:
+            print("Making request to Twilio to send a SMS")
+            response = sms.send_sms("The Current temperature sensor value is " + str(sensor_value))
+            print("Response received from Twilio is: " + str(response))
+            print("Status of SMS at Twilio is :" + str(response.status))
+    except Exception as e:
+        print("Error occured: Below are the details")
+        print(e)
+    time.sleep(10)
