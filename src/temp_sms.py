6f562aa7e16d93cfc45814809e7e7c955bdb63ac
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-04-13 20:12:36 +0530

    modified sms send in degree

--- src/temp_sms.py
+++ src/temp_sms.py
@@ -5,7 +5,7 @@ from boltiot import Bolt, Sms
 
 from src import conf
 
-minimum_limit = 300
+minimum_limit = 500
 maximum_limit = 600
 mybolt = Bolt(conf.API_KEY, conf.DEVICE_ID)
 sms = Sms(conf.SID, conf.AUTH_TOKEN, conf.TO_NUMBER, conf.FROM_NUMBER)
@@ -13,12 +13,12 @@ while True:
     print("Reading sensor value")
     response = mybolt.analogRead('A0')
     data = json.loads(response)
-    print("Sensor value is: " + str(data['value']))
+    sensor_value = int(data['value'])
+    print("Sensor value is: " + str((100 * sensor_value) / 1024))
     try:
-        sensor_value = int(data['value'])
         if sensor_value > maximum_limit or sensor_value < minimum_limit:
             print("Making request to Twilio to send a SMS")
-            response = sms.send_sms("The Current temperature sensor value is " + str(sensor_value))
+            response = sms.send_sms("The Current temperature sensor value is " + str((100 * sensor_value) / 1024))
             print("Response received from Twilio is: " + str(response))
             print("Status of SMS at Twilio is :" + str(response.status))
     except Exception as e:
