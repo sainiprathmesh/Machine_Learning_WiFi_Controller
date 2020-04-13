40fc06351e48016347def7098a1e779f5a7a649b
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-04-13 20:12:52 +0530

    range varied

--- src/temp_sms.py
+++ src/temp_sms.py
@@ -5,7 +5,7 @@ from boltiot import Bolt, Sms
 
 from src import conf
 
-minimum_limit = 500
+minimum_limit = 300
 maximum_limit = 600
 mybolt = Bolt(conf.API_KEY, conf.DEVICE_ID)
 sms = Sms(conf.SID, conf.AUTH_TOKEN, conf.TO_NUMBER, conf.FROM_NUMBER)
