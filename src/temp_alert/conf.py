e0a84ffa75b6b6e75dfd3f54b6ede3e3ba5b05fa
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-04-15 08:23:34 +0530

    details saved in variables

---
+++ src/temp_alert/conf.py
@@ -0,0 +1,6 @@
+"""Configurations for telegram_alert.py"""
+bolt_api_key = "a425c725-e879-4e29-a423-a9fd223e9e09"  # This is your Bolt Cloud API Key
+device_id = "BOLT14883313"  # This is the device ID and will be similar to BOLTXXXX where XXXX is some numbers
+telegram_chat_id = "@t.me/temperature_alert_saini"  # This is the channel ID of the created Telegram channel. Paste after @ symbol.
+telegram_bot_id = "t.me/saini_temp_alert_bot"  # This is the bot ID of the created Telegram Bot. Paste after bot text.
+threshold = 250  # Threshold beyond which the alert should be sent
