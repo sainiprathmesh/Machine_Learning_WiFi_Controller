3f8247a1ad51b24b1c6e66fe9c029069fef43bc9
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-04-15 08:28:12 +0530

    get_sensor_value_from_pin created for checking

--- src/temp_alert/telegram_alert.py
+++ src/temp_alert/telegram_alert.py
@@ -1,5 +1,24 @@
+import json
+
 from boltiot import Bolt
 
 from src.temp_alert import conf
 
 mybolt = Bolt(conf.bolt_api_key, conf.device_id)
+
+
+def get_sensor_value_from_pin(pin):
+    """Returns the sensor value. Returns -999 if request fails"""
+    try:
+        response = mybolt.analogRead(pin)
+        data = json.loads(response)
+        if data["success"] != 1:
+            print("Request not successfull")
+            print("This is the response->", data)
+            return -999
+        sensor_value = int(data["value"])
+        return sensor_value
+    except Exception as e:
+        print("Something went wrong when returning the sensor value")
+        print(e)
+        return -999
