73d2ec6c8ef0fbca3f96f0e128a000a67fd2397f
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-04-15 08:30:59 +0530

    send_telegram_message for sending message to telegram

--- src/temp_alert/telegram_alert.py
+++ src/temp_alert/telegram_alert.py
@@ -1,5 +1,6 @@
 import json
 
+import requests
 from boltiot import Bolt
 
 from src.temp_alert import conf
@@ -22,3 +23,28 @@ def get_sensor_value_from_pin(pin):
         print("Something went wrong when returning the sensor value")
         print(e)
         return -999
+
+
+def send_telegram_message(message):
+    """Sends message via Telegram"""
+    url = "https://api.telegram.org/" + conf.telegram_bot_id + "/sendMessage"
+    data = {
+        "chat_id": conf.telegram_chat_id,
+        "text": message
+    }
+    try:
+        response = requests.request(
+            "POST",
+            url,
+            params=data
+        )
+        print("This is the Telegram URL")
+        print(url)
+        print("This is the Telegram response")
+        print(response.text)
+        telegram_data = json.loads(response.text)
+        return telegram_data["ok"]
+    except Exception as e:
+        print("An error occurred in sending the alert message via Telegram")
+        print(e)
+        return False
